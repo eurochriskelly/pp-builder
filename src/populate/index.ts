ad54ecc397a195d739a992b8a2f7fa2291c3f629
Chris Kelly <eurochriskelly@gmail.com>
2024-05-05 21:09:08 +0200

    Update imports from core and add new organize subcommand

--- src/populate/index.ts
+++ src/populate/index.ts
@@ -1,15 +1,18 @@
 import readline from 'readline';
-import chalk from 'chalk';
 import * as yaml from 'js-yaml';
 import { validation } from 'gcp-core'
 import { writeFileSync, readFileSync } from 'fs';
 import Table from 'cli-table3'; // Updated import syntax for TypeScript
 
 const { validateFixtures } = validation;
+var tournamentData: any = {};
+const fixturesYamlPath = '/tmp/fixtures.yaml';
 
-const { green, yellow } = chalk;
+const green = (text: any) => `\x1b[32m${text}\x1b[0m`;
+const yellow = (text: any) => `\x1b[33m${text}\x1b[0m`;
 
 interface Fixture {
+    matchId: number;
     startTime: string;
     pitch: string;
     stage: string;
@@ -34,18 +37,37 @@ interface ITData {
     pitches: string[],
     stages: string[]
 } 
-const fixturesYamlPath = 'test/fixtures.yaml';
-const tournamentData: any = yaml.load(readFileSync(fixturesYamlPath, 'utf8'));
 
-export const recursiveAddFixture = () => {
+export const populate = async (schedule: string) => {
+    console.log(`Loading file [${schedule}]`)
+    const tdata: any = yaml.load(readFileSync(schedule, 'utf8')) as ITData;
+    console.log('tdata contains')
+    console.log(tdata)
+    const issues: string[] = [];
+    validateFixtures(tdata, issues);
+    if (issues.length > 0) {
+        console.log(yellow('Validation issues found in fixtures.yaml'))
+        issues.forEach(issue => console.error(issue));
+        console.log('')
+        process.exit(1);
+    }
+    tournamentData = tdata;
+    await recursiveAddFixture();
+}
+
+export const recursiveAddFixture = async () => {
     console.clear();
     const table = new Table({
-        head: ['StartTime', 'Pitch', 'Stage', 'Category', 'Group', 'Team1', 'Team2', 'Umpire', 'Duration'],
+        head: [
+          'StartTime', 'Pitch', 'Stage', 'Category',
+          'Group', 'Team1', 'Team2', 'Umpire', 'Duration'
+        ],
         colWidths: [11, 12, 12, 12, 10, 20, 20, 20, 12]
     });
 
-    tournamentData.schedule.fixtures.slice(-8).forEach((fixture: Fixture) => {
+    tournamentData.activities.forEach((fixture: Fixture) => {
         table.push([
+            fixture.matchId,
             fixture.startTime,
             fixture.pitch,
             fixture.stage,
@@ -58,57 +80,56 @@ export const recursiveAddFixture = () => {
         ]);
     });
     console.log(table.toString());
-    nextFixture(recursiveAddFixture);
+    await nextFixture(recursiveAddFixture);
 };
 
-const nextFixture = (callback: () => void) => {
-    const lastFixture = tournamentData.schedule.fixtures[tournamentData.schedule.fixtures.length - 1];
-    const newFixture = [...Object.values(lastFixture)];
-    newFixture[5] = "?"; 
-    newFixture[6] = "?"; 
-    newFixture[7] = "?"; 
+const nextFixture = async (callback: () => void) => {
+    const { categories, activities, pitches, stages } = tournamentData;
+    const defaultFixture: Fixture = { 
+        matchId: 0,
+        startTime: '08:00',
+        pitch: pitches[0].name,
+        stage: 'group',
+        category: Object.keys(categories)[0],
+        group: '1',
+        team1: '?',
+        team2: '?',
+        umpire: '?',
+        duration: 20 
+    }
+    const lastFixture = activities.length 
+        ? activities[activities.length - 1]
+        : defaultFixture;
 
-    function askForDetails(index: number) {
-        const teamName = (stage: string, exclude: string[] = [], allGroups = false): string[] => {
-            let teams: string[] = [];
-            if (stage === 'group') {
-                if (allGroups) {
-                    Object.keys(tournamentData.categories).forEach(categoryKey => {
-                        const categoryGroups = tournamentData.categories[categoryKey];
-                        Object.keys(categoryGroups).forEach(groupKey => {
-                            const prefixedTeams = categoryGroups[groupKey].map((teamName: string) => `${categoryKey}:${teamName}`);
-                            teams = teams.concat(prefixedTeams);
-                        });
-                    });
-                } else {
-                    const groups = tournamentData.categories[newFixture[3] as string];
-                    teams = groups[newFixture[4] as string];
-                }
-                return teams.filter(team => !exclude.some(excludeName => team.includes(excludeName))).sort();
-            } else {
-                return [
-                    // Placeholder options for other stages
-                ]
-            }
-        }
+    const newFixture: any = JSON.parse(JSON.stringify(lastFixture)) as Fixture;
+    newFixture.matchId = newFixture.matchId + 1;
+    newFixture.team1 = "?"; 
+    newFixture.team2 = "?"; 
+    newFixture.umpire = "?"; 
+
+    async function askForDetails(index: number) {
 
         // Define choices dynamically based on user input and scenario
         const questions = [
-            { label: 'START_TIME', value: addThirtyMinutes(newFixture[0] as string) },
-            { label: 'PITCH', value: newFixture[1], choices: tournamentData.pitches },
-            { label: 'STAGE', value: newFixture[2], choices: tournamentData.stages },
-            { label: 'CATEGORY', value: newFixture[3], choices: Object.keys(tournamentData.categories) },
-            { label: 'STAGE_ORDER', value: newFixture[4], choices: Object.keys(tournamentData.categories[newFixture[3] as string]) },
-            { label: 'TEAM1', value: newFixture[5], choices: teamName(newFixture[2] as string) },
-            { label: 'TEAM2', value: newFixture[6], choices: teamName(newFixture[2] as string, [newFixture[5] as string]) },
-            { label: 'UMPIRE', value: newFixture[7], choices: teamName(newFixture[2] as string, [newFixture[5] as string, newFixture[6] as string], true) },
-            { label: 'DURATION', value: newFixture[8], choices: [20, 15, 12, 10] },
+            { label: 'MATCH_ID', value: newFixture.matchId},
+            { label: 'START_TIME', value: addMinutes(newFixture.startTime, '30'), process: adjustMinutes },
+            { label: 'PITCH', value: newFixture.pitch, choices: pitches
+                .map((p:any) => p.name)
+                .filter((p:any, index:number) => categories[newFixture.category].pitches.includes(index))
+            },
+            { label: 'STAGE', value: newFixture.stage, choices: stages },
+            { label: 'CATEGORY', value: newFixture.category, choices: Object.keys(categories) },
+            { label: 'STAGE_ORDER', value: newFixture.group, choices: [1,2,3,4,5,6] },
+            { label: 'TEAM1', value: newFixture.team1, choices: teamName(categories, newFixture) },
+            { label: 'TEAM2', value: newFixture.team2, choices: teamName(categories, newFixture, [newFixture.team1]) },
+            { label: 'UMPIRE', value: newFixture.umpire, choices: teamName(categories, newFixture, [newFixture.team1, newFixture.team2], true) },
+            { label: 'DURATION', value: newFixture.duration, choices: [30, 20, 15, 12, 10] },
         ];
 
         if (index >= questions.length) {
             console.log("Complete Fixture: ", newFixture);
             writeFileSync(fixturesYamlPath, '    - ' + JSON.stringify(newFixture).replace(/,/g, ', ') + '\n', { flag: 'a' });
-            tournamentData.schedule.fixtures.push(newFixture as any);
+            activities.push(newFixture as any);
             callback();
             return;
         }
@@ -120,25 +141,66 @@ const nextFixture = (callback: () => void) => {
             prompt += ` [${question.choices.map((x: string, i: number) => `\x1b[33m${i + 1}\x1b[0m` + '.' + x).join(", ")}] `;
         }
 
+        const keys = Object.keys(newFixture);
         rl.question(prompt, (answer) => {
+            let ans
             if (answer.trim() !== '') {
                 const idxChoice = parseInt(answer, 10) - 1;
-                newFixture[index] = question.choices ? question.choices[idxChoice] : answer.trim();
+                ans = question.choices ? question.choices[idxChoice] : answer.trim();
             } else {
-                newFixture[index] = question.value;
+                ans = question.value;
+            }
+            if (!question.process) {
+                newFixture[keys[index]] = ans;
+            } else {
+                newFixture[keys[index]] = question.process(newFixture, ans);
             }
             askForDetails(index + 1);
         });
     }
-    askForDetails(0);
+    await askForDetails(0);
 }
 
-const addThirtyMinutes = (time: string): string => {
+export const teamName = (
+    categories: any,
+    fixture: Fixture,
+    exclude: string[] = [],
+    allGroups = false
+): string[] => {
+    const { category, stage } = fixture;
+    let currteams: string[] = [];
+    if (stage === 'group') {
+        if (allGroups) {
+            Object.keys(categories).forEach(categoryKey => {
+                const categoryGroups = categories[categoryKey];
+                Object.keys(categoryGroups).forEach(groupKey => {
+                    const prefixedTeams = categoryGroups[groupKey].map((teamName: string) => `${categoryKey}:${teamName}`);
+                    currteams = currteams.concat(prefixedTeams);
+                });
+            });
+        } else {
+            const { groups, teams } = categories[category];
+        }
+        return []
+    } else {
+        return [
+            // Placeholder options for other stages
+        ]
+    }
+}
+
+const adjustMinutes = (newFixtures: Fixture, ans: string) => {
+    if (ans[0] === '-' || ans[0] === '+') {
+        return addMinutes(newFixtures.startTime, ans);
+    }
+    return ans;
+}
+
+const addMinutes = (time: string, offset: string): string => {
     const parts = time.split(':');
     let hours = parseInt(parts[0], 10);
     let minutes = parseInt(parts[1], 10);
-
-    minutes += 30;
+    minutes += parseInt(offset);
 
     if (minutes >= 60) {
         hours += 1;
@@ -154,6 +216,4 @@ const addThirtyMinutes = (time: string): string => {
 
     return `${formattedHours}:${formattedMinutes}`;
 }
-
-
 
