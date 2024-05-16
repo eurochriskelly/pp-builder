c02b42ffbe711faf64a6ffe0bb30289489d7a523
Chris Kelly <eurochriskelly@gmail.com>
2024-05-16 15:19:03 +0200

    Remove header row from export if it exists

--- src/import/index.ts
+++ src/import/index.ts
@@ -37,29 +37,27 @@ const generateFixturesImport = (data: any) => {
   const { tournamentId, startDate } = data
   const rows = [
     'DELETE FROM `EuroTourno`.`fixtures` WHERE `tournamentId` = ' + tournamentId + ';',
-    ...data.activities.map((fixture: any) => {
-      const [id, time, pitch, stage, category, group, team1, team2, umpireTeam] = fixture;
-      return [
-        "INSERT INTO `EuroTourno`.`fixtures` (",
-        " `id`, `tournamentId`, `category`, `groupNumber`, `stage`, `pitch`, ",
-        " `scheduled`, `started`, ",
-        " `team1Planned`, `team1Id`, `goals1`, `points1`,",
-        " `team2Planned`, `team2Id`, `goals2`, `points2`, ",
-        " `umpireTeamPlanned`, `umpireTeamId` ",
-        ") VALUES ( ",
-        ` '${id}', '${tournamentId}', '${category}', '${group}', '${stage}', '${pitch}', `,
-        ` '${startDate} ${time}:00', NULL, `,
-        ` '${team1}', '${team1}', NULL, NULL, `,
-        ` '${team2}', '${team2}', NULL, NULL, `,
-        ` '${umpireTeam}', '${umpireTeam}'`,
-        ");"
-      ].join('');
-    })
+    ...data.activities
+      .filter((row: any) => row[0] !== 'matchId') // remove header ow if it exists
+      .map((fixture: any) => {
+        const [id, time, pitch, stage, category, group, team1, team2, umpireTeam] = fixture;
+        return [
+          "INSERT INTO `EuroTourno`.`fixtures` (",
+          " `id`, `tournamentId`, `category`, `groupNumber`, `stage`, `pitch`, ",
+          " `scheduled`, `started`, ",
+          " `team1Planned`, `team1Id`, `goals1`, `points1`,",
+          " `team2Planned`, `team2Id`, `goals2`, `points2`, ",
+          " `umpireTeamPlanned`, `umpireTeamId` ",
+          ") VALUES ( ",
+          ` '${id}', '${tournamentId}', '${category}', '${group}', '${stage}', '${pitch}', `,
+          ` '${startDate} ${time}:00', NULL, `,
+          ` '${team1}', '${team1}', NULL, NULL, `,
+          ` '${team2}', '${team2}', NULL, NULL, `,
+          ` '${umpireTeam}', '${umpireTeam}'`,
+          ");"
+        ].join('');
+      })
   ];
   return rows.join('\n');
 }
 
-/*
-INSERT INTO `EuroTourno`.`fixtures` ( `tournamentId`, `category`, `groupNumber`, `stage`, `pitch`, `scheduled`, `started`, `team1Planned`, `team1Id`, `goals1`, `points1`, `team2Planned`, `team2Id`, `goals2`, `points2`, `umpireTeamPlanned`, `umpireTeamId` ) VALUES ( '6', 'group', 'Mens', 'Pitch 5', '10:30',  '2024-03-23 1:00', NULL,  '1', '1', NULL, NULL,  'Amsterdam A', 'Amsterdam A', NULL, NULL,  'Leuven A', 'Leuven A');
-INSERT INTO `EuroTourno`.`fixtures` ( `tournamentId`, `category`, `groupNumber`, `stage`, `pitch`, `scheduled`, `started`, `team1Planned`, `team1Id`, `goals1`, `points1`, `team2Planned`, `team2Id`, `goals2`, `points2`, `umpireTeamPlanned`, `umpireTeamId`)  VALUES ( '5', 'Ladies', '1', 'finals', 'Z1', '2023-03-02 14:00:00', NULL, '~Ladies_group1/p:1', '~Ladies_group1/p:1', NULL, NULL, '~Ladies_group1/p:2', '~Ladies_group1/p:2', NULL, NULL, '~Ladies_group1/p:2', '~Ladies_group1/p:2');
-*/ 
