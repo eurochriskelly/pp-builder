f38479e0e4dc4343a62c7884ff85d3db2b3367eb
Chris Kelly <eurochriskelly@gmail.com>
2024-06-13 13:59:05 +0200

    Add a simple validation process
    Checks that number of matches match expected based on what was extracted

---
+++ src/import/validate.spec.ts
@@ -0,0 +1,15 @@
+import { validateRows, getCategories } from './validate';
+
+
+
+describe('test row validation', () => {
+  it('check for correct number of teams', () => {
+    const categories = getCategories(rows);
+    const rows = [
+
+    ]
+  })
+})
+
+
+
