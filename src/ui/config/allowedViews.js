7c5704491d820e9a650df223a2402ae6791d6cde
Chris Kelly <eurochriskelly@gmail.com>
2025-03-08 13:56:27 +0100

    Fixed some broken views
    borken by a previous grok session

--- src/ui/config/allowedViews.js
+++ src/ui/config/allowedViews.js
@@ -1,7 +1,20 @@
 const generateRecentView = require('../templates/views/execution/recent');
 const generateGroupFixtures = require('../templates/views/execution/groupFixtures');
+const generateGroupStandings = require('../templates/views/execution/groupStandings');
 const generateKnockoutFixtures = require('../templates/views/execution/knockoutFixtures');
-const { getRecentMatches, getGroupFixtures, getKnockoutFixtures } = require('../queries');
+const generateCardedPlayers = require('../templates/views/execution/cardedPlayers');
+const generateMatchesByPitch = require('../templates/views/execution/matchesByPitch');
+const generateFinalsResults = require('../templates/views/execution/finalsResults');
+
+const { 
+    getRecentMatches, 
+    getGroupFixtures, 
+    getGroupStandings,
+    getKnockoutFixtures,
+    getCardedPlayers,
+    getMatchesByPitch,
+    getFinalsResults
+} = require('../queries/matches');
 
 const allowedViews = {
     'recent': {
@@ -14,10 +27,25 @@ const allowedViews = {
         generator: generateGroupFixtures,
         fetch: getGroupFixtures
     },
+    'view3': {
+        title: 'Group Standings',
+        generator: generateGroupStandings,
+        fetch: getGroupStandings
+    },
     'view4': {
         title: 'Knockout Fixtures',
         generator: generateKnockoutFixtures,
         fetch: getKnockoutFixtures
+    },
+    'view6': {
+        title: 'Matches by Pitch',
+        generator: generateMatchesByPitch,
+        fetch: getMatchesByPitch
+    },
+    'view7': {
+        title: 'Finals Results',
+        generator: generateFinalsResults,
+        fetch: getFinalsResults
     }
 };
 
