a21490bec7db39d67b60f4219e70428733ba6440
Chris Kelly <eurochriskelly@gmail.com>
2025-03-25 20:17:32 +0100

    last of the tweaks

--- src/ui/templates/views/execution/groupStandings.js
+++ src/ui/templates/views/execution/groupStandings.js
@@ -28,19 +28,19 @@ module.exports = function generateGroupStandings(data) {
                 </colgroup>
                 <tr><th colspan="8" class="group-header">Group ${groupData.groupName}</th></tr>
                 <tr>${['Team', 'Played', 'Wins', 'Draws', 'Losses', 'Scores For', 'Score diff', 'Points'].map((h, i) => 
-                    `<th class="table-header ${i === 0 ? 'text-left' : 'text-right'}">${h}</th>`
+                    `<th class="table-header ${i === 0 ? 'text-left' : 'vertical-head'}">${h}</th>`
                 ).join('')}</tr>
             `;
             for (const row of groupData.rows) {
                 html += '<tr>';
-                html += `<td style="text-align: left;">${row.team}</td>`;
-                html += `<td style="text-align: right;">${row.MatchesPlayed}</td>`;
-                html += `<td style="text-align: right;">${row.Wins}</td>`;
-                html += `<td style="text-align: right;">${row.Draws}</td>`;
-                html += `<td style="text-align: right;">${row.Losses}</td>`;
-                html += `<td style="text-align: right;">${row.PointsFrom}</td>`;
-                html += `<td style="text-align: right;">${row.PointsDifference}</td>`;
-                html += `<td style="text-align: right;">${row.TotalPoints}</td>`;
+                html += `<td class="uppercase font-bold" style="text-align: left;">${row.team}</td>`;
+                html += `<td style="text-align: center;">${row.MatchesPlayed}</td>`;
+                html += `<td style="text-align: center;">${row.Wins}</td>`;
+                html += `<td style="text-align: center;">${row.Draws}</td>`;
+                html += `<td style="text-align: center;">${row.Losses}</td>`;
+                html += `<td style="text-align: center;">${row.PointsFrom}</td>`;
+                html += `<td style="text-align: center;">${row.PointsDifference}</td>`;
+                html += `<td style="text-align: center;">${row.TotalPoints}</td>`;
                 html += '</tr>';
             }
             html += '</table>';
